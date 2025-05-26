b407b1ac6b7842c2496272b5de75a193cca81f4a
programmerviva <sd.vikasvaibhav@gmail.com>
2025-05-26 13:25:13 +0530

    fix eslint

--- client/src/components/EmployeeDashboardSummary.jsx
+++ client/src/components/EmployeeDashboardSummary.jsx
@@ -1,8 +1,10 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-unused-vars */
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import dayjs from 'dayjs';
 import AttendanceStats from './AttendanceStats';
+
 
 const apiUrl = import.meta.env.VITE_API_URL || 'http://localhost:5000';
 
